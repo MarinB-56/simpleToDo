0680a191864a0df0d1347ee3efacbf7b918254c6
Marin Boone <150065021+MarinB-56@users.noreply.github.com>
2025-02-23 17:03:52 +0100

    Add middleware to handle data sent to the server

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
 const express = require('express');
+const path = require('path');
 const morgan = require('morgan');
-const app = express();
 const port = 3000;
 
+const app = express();
+
+// Middleware
+app.use(morgan('dev')); // pour avoir l'url dans la console
+app.use(express.json()); // pour transformer les données envoyées dans le body d'une requête HTTP en json propre
+app.use(express.urlencoded({ extended : true })); // Pour récupérer les données d'un form envoyé au serveur
+
+// Servir les fichiers statiques
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Import des routes de tasks
 const tasksRoutes = require('./src/routes/tasks');
-
-app.use(morgan('dev'));
-
 app.use('/tasks', tasksRoutes);
 
+// route par défaut
 app.get('/', (req, res) => {
-  res.send("Coucouilles");
+  res.send("simple Todo");
 })
 
 app.listen(port, () => {
